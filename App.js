af5d779b76a09bcbe966ff051cd2a89867681529
mazin <mazink2@gmail.com>
2024-01-15 00:01:08 -0500

    Created search component UI

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from "./src/components/Header";
+import Search from "./src/components/Search";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <Text>Open up App.js to start working on your app!</Text>
+      <Search />
       <StatusBar style="auto" />
     </View>
   );
