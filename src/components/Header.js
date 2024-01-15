c73b2ec98bf0578edaa8dd78b0e2a9bbc26569f4
mazin <mazink2@gmail.com>
2024-01-14 22:33:12 -0500

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.lightHeader}>Grab your</Text>
+      <Text style={styles.oldHeader}>Delicious meal!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+    marginHorizontal: 25
+  },
+  lightHeader: {
+    fontSize: 35,
+  },
+  oldHeader: {
+    fontSize: 40,
+    fontWeight: "bold",
+  }
+})
